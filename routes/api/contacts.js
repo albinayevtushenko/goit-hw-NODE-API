325b543c5b5f07971ee518ee83b8c8b40af99ad9
Albina Yevtushenko <109302599+albinayevtushenko@users.noreply.github.com>
2023-08-23 12:53:34 +0300

    add some changes

---
+++ routes/api/contacts.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const ctrl = require("../../controllers/contacts");
+const { schemas } = require("../../models/contact");
+const { validateBody, isValidId } = require("../../middlewares");
+
+router.get("/", ctrl.getAll);
+
+router.get("/:contactId", isValidId, ctrl.getById);
+
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
+
+router.delete("/:contactId", isValidId, ctrl.removeById);
+
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateById
+);
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateStatusContact
+);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const { DB_URI, PORT } = process.env;
+
+mongoose
+  .connect(DB_URI)
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
