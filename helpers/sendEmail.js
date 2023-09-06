8b6ce3cca7225b45e5001b1304afd1026047c3dd
Albina Yevtushenko <109302599+albinayevtushenko@users.noreply.github.com>
2023-09-06 14:52:45 +0300

    fixed hw6-email

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -16,7 +16,7 @@ const nodemailerConfig = {
 const transport = nodemailer.createTransport(nodemailerConfig);
 
 const sendEmail = async (data) => {
-  const email = { ...data, from: "ludmilanikitenko0703@meta.ua" };
+  const email = { ...data, from: "albina.yevtushenko@meta.ua" };
   await transport.sendMail(email);
   return true;
 };
